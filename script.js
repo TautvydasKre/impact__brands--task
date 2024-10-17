8d530f154859708c203e9ebec74e159c3339b67d
TautvydasKre <144778291+TautvydasKre@users.noreply.github.com>
2024-10-17 15:35:40 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//////////////////////////////////
+///<-- Carousel code -->///
+//////////////////////////////////
+
 let currentSlide = 0;
 const slides = document.querySelectorAll(".slide");
 const totalSlides = slides.length;
@@ -23,3 +27,27 @@ function changeSlide(direction) {
 
 slides[currentSlide].classList.add("active");
 bubbles[currentSlide].classList.add("active");
+
+//////////////////////////////////
+///<-- FAQ'S code -->///
+//////////////////////////////////
+
+document.querySelectorAll(".faq-question").forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
+
+    question.classList.toggle("active");
+  });
+});
+
+//////////////////////////////////
+///<-- Burger code -->///
+//////////////////////////////////
+
+const burger = document.querySelector(".burger");
+const navList = document.querySelector(".main__nav-list");
+
+burger.addEventListener("click", () => {
+  navList.classList.toggle("active"); // Toggle the active class on click
+});
